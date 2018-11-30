d39da9b0965b2c71dd870026e282b13f7684f9be
Amillz70 <adammiller7@yahoo.com>
2018-11-30 10:33:44 -0500

    Link production with heroku server
    server domain https://whispering-eyrie-15559.herokuapp.com/

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 let apiUrl
 const apiUrls = {
-  production: '<replace-with-heroku-url>',
+  production: 'https://whispering-eyrie-15559.herokuapp.com/',
   development: 'http://localhost:4741'
 }
 
